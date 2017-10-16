d469439e86621fcbac80c93cad5d33899b3fda42
Arun <shan.arun@gmail.com>
2017-10-16 20:52:58 +0100

    Sequence of Options

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,8 +1,20 @@
 class OptionsController < ApplicationController
+  include OptionsHelper
   def index
     @options = Option.all
   end
   def show
     @options = Option.find(params[:id])
+  end
+  def new
+    @options = Option.new
+  end
+  def create
+    @options = Option.new(option_params)
+    @options.count0 = 0
+    @options.count1 = 0
+    @options.id = maxoption + 1
+    @options.save
+    redirect_to option_path(@options)
   end
 end
--- app/helpers/options_helper.rb
+++ app/helpers/options_helper.rb
@@ -1,2 +1,8 @@
 module OptionsHelper
+  def option_params
+  params.require(:option).permit(:id, :option0, :option1, :count0, :count1)
+end
+def maxoption
+   Option.last.id
+ end
 end
